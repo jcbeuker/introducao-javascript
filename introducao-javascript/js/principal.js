7a0a6ed9a4fdcc0abe9cfe7c552410a18adf1660
JOSE CAETANO BEUKER <jcbeuker@yahoo.com.br>
2018-11-03 17:08:08 -0300

    Início JS no git

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,12 @@
+var titulo = (document.querySelector(".titulo"));
+// var titulo = (document.getElementById(".idTitulo"));
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = (document.querySelector("#paciente_0"));
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
