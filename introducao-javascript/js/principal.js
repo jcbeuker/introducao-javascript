14110c23fd4558f89dffde313258a59502c529cb
JOSE CAETANO BEUKER <jcbeuker@yahoo.com.br>
2018-11-05 22:40:18 -0200

    Operadores lógicos

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -6,7 +6,22 @@ var paciente = (document.querySelector("#paciente_0"));
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 var altura = paciente.querySelector(".info-altura").textContent;
-var imc = peso / (altura * altura);
-
 var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso > 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido.";
+}
+
+if (altura <= 0 || altura > 3) {
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida.";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
