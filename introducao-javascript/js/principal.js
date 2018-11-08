c7c5297c118fd669e54e5671c03e5eb5d6b31376
JOSE CAETANO BEUKER <jcbeuker@yahoo.com.br>
2018-11-08 17:52:04 -0200

    4 - Evitando comportamento padrão ==> função event.preventDefault

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -36,6 +36,7 @@ for (i = 0; i < pacientes.length; i++) {
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
-botaoAdicionar.addEventListener("click", function () {
-    console.log("Cliquei!");
+botaoAdicionar.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Fui clicado!");
 });
