a14dfc90c82bee74c80d8f886b17abfb6b23d868
JOSE CAETANO BEUKER <jcbeuker@yahoo.com.br>
2018-11-10 16:37:44 -0200

    Quebrando o código em arquivos separados.

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,35 @@
+var titulo = (document.querySelector(".titulo"));
+// var titulo = (document.getElementById(".idTitulo"));
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+
+    if (peso <= 0 || peso > 1000) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido.";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura > 3) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida.";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
